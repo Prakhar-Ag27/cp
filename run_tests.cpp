2dc4acbb006aae364c2b638efc2d564dd98bf4f1
Prakhar Agrawal <agrawal.prakhar35@gmail.com>
2026-06-27 18:46:18 +0000

    Add file redirection

--- run_tests.cpp
+++ run_tests.cpp
@@ -1,9 +1,19 @@
+#include <fcntl.h>
 #include <iostream>
 #include <string>
 #include <unistd.h>
 
 int main() {
-  std::string fileName = "./solution.cpp";
+  int inputFileFd = open("input.txt", O_RDONLY);
+  int outputFileFd = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+  dup2(inputFileFd, 0);
+  dup2(outputFileFd, 1);
+
+  close(inputFileFd);
+  close(outputFileFd);
+
+  std::string fileName = "./bin/solution";
   char *args[] = {(char *)"solution", nullptr};
   if (auto err = execv(fileName.c_str(), args)) {
     std::cout << "Error in executing the provided file.\n"
--- solution.cpp
+++ solution.cpp
@@ -1,22 +1,13 @@
-#include <algorithm>
-#include <cmath>
 #include <iostream>
-#include <vector>
-using namespace std;
 using ll = long long;
 
-bool isPossible(vector<int> &v, int k, int ele) {
-  int count = 0;
-  for (int i = 1; i <= v.size() - k; i++) {
-    if (v[i] == ele && v[i] != v[i + k])
-      count++;
-  }
-  return count;
-}
-
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  std::cout << "TESTING!!" << "\n";
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::string s;
+  int x, y;
+  std::cin >> s;
+  std::cin >> x >> y;
+  std::cout << s << "\n" << x << " " << y << "\n";
   return 0;
 }
